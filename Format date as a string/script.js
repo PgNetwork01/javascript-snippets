9a4a41ddc975e275ee5773058cf1404f6b7a39ea
Pg Network <gamerzp780@gmail.com>
2024-06-12 10:23:37 +0530

    Create script.js

---
+++ Format date as a string/script.js
@@ -0,0 +1,7 @@
+function formatDate(date) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(date).toLocaleDateString(undefined, options);
+}
+
+// Usage
+console.log(formatDate('2022-06-12'));
